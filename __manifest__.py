c7605dd27177a9c7c2aa601b1b8a0c50702d8813
MBA Consultings <devops@mbaconsultings.com>
2026-02-20 09:57:28 -0500

    fixes varios

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Formato Cotizacion Suplidora JC',
+    'version': '1.0',
+    'summary': 'Custom Sale Order Report for Suplidora JC',
+    'description': 'Replicates the design of _COT 285 PETROLEOS DELTA.pdf for Sale Orders.',
+    'category': 'Sales',
+    'author': 'Antigravity',
+    'depends': ['sale', 'facturacion_electronica'],
+    'data': [
+        'views/report_saleorder.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
