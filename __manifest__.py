8be786c8480ca274c92c8c45ac724b3722390cf6
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-06-24 08:58:16 -0500

    Update module

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,7 @@
 {
     'name': 'Formato Cotizacion Suplidora JC',
-    'version': '1.0',
+    'version': '19.0.1.0.0',
+    'license': 'LGPL-3',
     'summary': 'Custom Sale Order Report for Suplidora JC',
     'description': 'Replicates the design of _COT 285 PETROLEOS DELTA.pdf for Sale Orders.',
     'category': 'Sales',
