ec0c8fc8aa1bb610441adfb180f8c49ad76c9ffe
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-06-24 09:20:32 -0500

    Update description

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'version': '19.0.1.0.0',
     'license': 'LGPL-3',
     'summary': 'Custom Sale Order Report for Suplidora JC',
-    'description': 'Replicates the design of _COT 285 PETROLEOS DELTA.pdf for Sale Orders.',
+    'description': 'Base quote format for Sale Orders.',
     'category': 'Sales',
     'author': 'Antigravity',
     'depends': ['sale'],
