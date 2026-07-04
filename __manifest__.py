19aac21e480dada311df39c5faa74ea2fba347e2
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-07-03 19:25:13 -0500

    Revert "Fix version compatibility for Odoo 18"
    This reverts commit 69686903338d57c81ae5d041dfb48045ec948b64.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Formato Cotizacion',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1.0.0',
     'license': 'LGPL-3',
     'summary': 'Base Custom Sale Order Report Template',
     'description': 'Base quote format for Sale Orders, easily customizable for clients.',
